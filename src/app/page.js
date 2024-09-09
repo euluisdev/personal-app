db5c687502873413a44ba30f5f64afd9574b4dbf
euluis-dev <fluisf00@gmail.com>
2024-09-09 16:18:41 -0300

    update page.js root to to get style

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState } from 'react';
-import styles from './page.module.css'; // Importando o arquivo CSS
+import styles from './page.css'; // Importando o arquivo CSS
 
 export default function HomePage() {
   const [formType, setFormType] = useState('login'); // Alterna entre login e cadastro
@@ -67,150 +67,174 @@ export default function HomePage() {
   };
 
   return (
-    <div className={styles.container}>
-      <h1>{formType === 'login' ? 'Login' : 'Cadastro'}</h1>
-      <form onSubmit={handleSubmit}>
-        {formType === 'register' && (
-          <>
-            <div>
-              <label>Nome:</label>
-              <input
-                type="text"
-                name="nome"
-                value={formData.nome}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Data de Nascimento:</label>
-              <input
-                type="date"
-                name="datanasc"
-                value={formData.datanasc}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Sexo:</label>
-              <input
-                type="radio"
-                name="sexo"
-                value="masculino"
-                checked={formData.sexo === 'masculino'}
-                onChange={handleChange}
-                required
-              /> Masculino
-              <input
-                type="radio"
-                name="sexo"
-                value="feminino"
-                checked={formData.sexo === 'feminino'}
-                onChange={handleChange}
-                required
-              /> Feminino
-            </div>
-            <div>
-              <label>Peso (kg):</label>
-              <input
-                type="number"
-                name="peso"
-                value={formData.peso}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Altura (cm):</label>
-              <input
-                type="number"
-                name="altura"
-                value={formData.altura}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Alergia:</label>
-              <input
-                type="text"
-                name="alergia"
-                value={formData.alergia}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Lesão:</label>
-              <input
-                type="text"
-                name="lesao"
-                value={formData.lesao}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>É iniciante?:</label>
-              <input
-                type="text"
-                name="iniciante"
-                value={formData.iniciante}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Objetivo:</label>
-              <input
-                type="text"
-                name="objetivo"
-                value={formData.objetivo}
-                onChange={handleChange}
-                required
-              />
-            </div>
-            <div>
-              <label>Horário disponível do treino (horas):</label>
-              <input
-                type="number"
-                name="hora"
-                value={formData.hora}
-                onChange={handleChange}
-                required
-              />
-            </div>
-          </>
-        )}
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label>Senha:</label>
-          <input
-            type="password"
-            name="senha"
-            value={formData.senha}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">
-          {formType === 'login' ? 'Entrar' : 'Cadastrar'}
-        </button>
-      </form>
-      <p>{message}</p>
-      <button className={styles.toggle} onClick={() => setFormType(formType === 'login' ? 'register' : 'login')}>
-        {formType === 'login' ? 'Criar Conta' : 'Já tenho conta'}
-      </button>
+    <div className='modal-overlay'>
+      <div className="modal-container">
+        <h1 className="modal-title">{formType === 'login' ? 'Login' : 'Cadastro'}</h1>
+        <form onSubmit={handleSubmit}>
+          {formType === 'register' && (
+            <>
+              <div>
+                <label>Nome:</label>
+                <input
+                  type="text"
+                  name="nome"
+                  value={formData.nome}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Data de Nascimento:</label>
+                <input
+                  type="date"
+                  name="datanasc"
+                  value={formData.datanasc}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Sexo:</label>
+                <input
+                  type="radio"
+                  name="sexo"
+                  value="masculino"
+                  checked={formData.sexo === 'masculino'}
+                  onChange={handleChange}
+                  required
+                /> Masculino
+                <input
+                  type="radio"
+                  name="sexo"
+                  value="feminino"
+                  checked={formData.sexo === 'feminino'}
+                  onChange={handleChange}
+                  required
+                /> Feminino
+              </div>
+              <div>
+                <label>Peso (kg):</label>
+                <input
+                  type="number"
+                  name="peso"
+                  value={formData.peso}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Altura (cm):</label>
+                <input
+                  type="number"
+                  name="altura"
+                  value={formData.altura}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Alergia:</label>
+                <input
+                  type="text"
+                  name="alergia"
+                  value={formData.alergia}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Lesão:</label>
+                <input
+                  type="text"
+                  name="lesao"
+                  value={formData.lesao}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>É iniciante?:</label>
+                <input
+                  type="text"
+                  name="iniciante"
+                  value={formData.iniciante}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Objetivo:</label>
+                <input
+                  type="text"
+                  name="objetivo"
+                  value={formData.objetivo}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div>
+                <label>Horário disponível do treino (horas):</label>
+                <input
+                  type="number"
+                  name="hora"
+                  value={formData.hora}
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+            </>
+          )}
+          <div>
+            <label>Email:</label>
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label>Senha:</label>
+            <input
+              type="password"
+              name="senha"
+              value={formData.senha}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">
+            {formType === 'login' ? 'Entrar' : 'Cadastrar'}
+          </button>
+          <p>
+            {formType === 'login' ? (
+              <>
+                Não tem uma conta?{' '}
+                <button
+                  type="button"
+                  className={styles.toggle}
+                  onClick={() => setFormType('register')}
+                >
+                  Cadastrar
+                </button>
+              </>
+            ) : (
+              <>
+                Já tem uma conta?{' '}
+                <button
+                  type="button"
+                  className={styles.toggle}
+                  onClick={() => setFormType('login')}
+                >
+                  Entrar
+                </button>
+              </>
+            )}
+          </p>
+          {message && <p>{message}</p>}
+        </form>
+      </div>
     </div>
   );
 }
