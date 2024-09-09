c1495e54672794c20da354abaf38a90363b2278c
euluis-dev <fluisf00@gmail.com>
2024-09-09 11:05:34 -0300

    project initialized -- folder structures -- ok

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,16 +1,5 @@
-import localFont from "next/font/local";
+import NavBar from "@/components/NavBar";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata = {
   title: "Create Next App",
@@ -19,9 +8,10 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+    <html lang="pt-br">
+      <body>
+        <NavBar />
+        <main>{children}</main> 
       </body>
     </html>
   );
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div className='nav-bar'>
+      <p>NavBar</p>
+    </div>
+  )
+}
+
+export default NavBar;
