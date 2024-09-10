da3918df07d38dd429aa56709a5e28281172516e
euluis-dev <fluisf00@gmail.com>
2024-09-10 15:02:28 -0300

    changed name file  -- Page dashboard

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,19 +1,28 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation'; // Verifique se o import está correto
+import { useState, useEffect } from 'react';
 
-const page = () => {
-    const searchParams = useSearchParams();
-    const userName = searchParams.get('userName') || 'Usuário'; // Obtém o nome passado pela URL
+const Page =() => {
+  const searchParams = useSearchParams();
+  const userName = searchParams.get('userName');
 
-    return (
-        <>
-            <div style={{ opacity: 1, display: 'block', position: 'relative', zIndex: 10 }} >
-                <h1>Bem-vindo, {userName}!</h1>
-                <p>Estamos felizes em tê-lo de volta. Aproveite seu treino!</p>
-            </div>
-        </>
-    );
-};
+  const [data, setData] = useState(null);
 
-export default page;  
- 
+  useEffect(() => {
+
+    if (userName) {
+      console.log(`UserName from query: ${userName}`);
+    }
+  }, [userName]);
+
+  return (
+    <>
+        <div style={{ opacity: 1, display: 'block', position: 'relative', zIndex: 10 }} >
+            <h1>Bem-vindo, {userName}!</h1>
+            <p>Estamos felizes em tê-lo de volta. Aproveite seu treino!</p>
+        </div>
+    </>
+);
+}
+
+export default Page;
