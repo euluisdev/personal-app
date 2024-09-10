8c3fd2c84120a9829fca400c2a5556b4ec600041
euluis-dev <fluisf00@gmail.com>
2024-09-10 14:28:18 -0300

    add feature routes in the next.config.mjs file

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,28 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/old-route',
+        destination: '/new-route',
+        permanent: true, // Define se o redirecionamento é permanente ou temporário
+      },
+      {
+        source: '/old-dashboard',
+        destination: '/dashboard',
+        permanent: false, // Redirecionamento temporário
+      },
+    ];
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://external-api.com/:path*', // Exemplo de reescrita para proxy
+      },
+    ];
+  },
+};
 
 export default nextConfig;
+
