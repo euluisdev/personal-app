2680f482f439e32db32ad7e2a8d7134923ba2298
euluis-dev <fluisf00@gmail.com>
2024-09-18 10:37:43 -0300

    fixing bug: list with pending users with status 500

--- src/app/api/approve-user/route.js
+++ src/app/api/approve-user/route.js
@@ -8,39 +8,40 @@ dotenv.config();
 const uri = process.env.MONGODB_URI;
 if (!uri) {
   throw new Error('MongoDB URI não está definida.');
-}
+}; 
+
+const jwtSecret = process.env.JWT_SECRET;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 export async function POST(req) {
   try {
-    // Verifica se o token de autorização está presente
+    //verifica se o token de autorização está presente
     const token = req.headers.authorization.split(' ')[1];
     if (!token) {
       return new Response(JSON.stringify({ message: 'Autorização necessária.' }), { status: 401 });
     }
 
-    // Verifica o token JWT
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    //verifica o token JWT
+    const decoded = jwt.verify(token, jwtSecret);
     if (decoded.role !== 'admin') {
       return new Response(JSON.stringify({ message: 'Acesso não autorizado.' }), { status: 403 });
     }
 
-    // Obtém o e-mail do corpo da requisição
+    //obtém o e-mail do corpo da requisição
     const { email } = await req.json();
     if (!email) {
       return new Response(JSON.stringify({ message: 'E-mail do usuário é necessário.' }), { status: 400 });
     }
 
-    // Conecta ao banco de dados
     await client.connect();
     const db = client.db('BestFitData');
     const collection = db.collection('users');
 
-    // Atualiza o status do usuário para 'aprovado'
+    //atualiza o status do usuário para 'aprovado'
     const result = await collection.updateOne(
-      { email },  // Critério de busca (usuário com o e-mail fornecido)
-      { $set: { status: 'aprovado' } }  // Atualização (altera o status para 'aprovado')
+      { email },  //critério de busca
+      { $set: { status: 'aprovado' } }  //atualização  
     );
 
     if (result.matchedCount === 0) {
--- src/app/api/list-users/route.js
+++ src/app/api/list-users/route.js
@@ -1,18 +1,25 @@
-// /pages/api/list-users.js
+// /app/api/list-users/route.js
 import { MongoClient } from 'mongodb';
 import jwt from 'jsonwebtoken';
+
+const uri = process.env.MONGODB_URI;
+if (!uri) {
+  throw new Error('Variável não definida');
+}; 
+
+const jwtSecret = process.env.JWT_SECRET;
 
 export async function GET(req) {
   const token = req.headers.authorization.split(' ')[1]; //obtém o token enviado no header
   
   try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const decoded = jwt.verify(token, jwtSecret);
     
     if (decoded.role !== 'admin') {
       return new Response(JSON.stringify({ message: 'Acesso não autorizado' }), { status: 403 });
     }
 
-    const client = new MongoClient(process.env.MONGODB_URI);
+    const client = new MongoClient(uri);
     await client.connect();
     const collection = client.db('BestFitData').collection('users');
     
