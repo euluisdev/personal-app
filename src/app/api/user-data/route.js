c94378e0bc970c98d3f91cccbab05b380081879b
euluis-dev <fluisf00@gmail.com>
2024-09-19 09:55:51 -0300

    I made the authorization header in the GET function, and the condition for Tokin validation

--- src/app/api/user-data/route.js
+++ src/app/api/user-data/route.js
@@ -4,3 +4,19 @@ import jwt from 'jsonwebtoken';
 const uri = process.env.MONGODB_URI; 
 const jwtSecret = process.env.JWT_SECRET; 
 
+export async function GET(req) {
+    const authHeader = req.headers.get('authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer')) {
+        return new Response(JSON.stringify({ message: 'Tokin não fornecido' }), { status: 401 }); 
+    };
+};
+
+
+try {
+    
+} catch (error) {
+    console.error('Erro ao buscar dados do usuário:', error);
+    return new Response(JSON.stringify({ message: 'Erro interno do servidor' }), { status: 500 });
+}; 
+ 
