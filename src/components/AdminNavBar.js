43256c694e6426cbd64f74796e60b920120cf22f
euluis-dev <fluisf00@gmail.com>
2024-09-27 12:00:42 -0300

    admin navbar component was created

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -2,9 +2,17 @@ import React from 'react';
 
 const AdminNavBar = () => {
   return (
-    <div>
-      navbar
-    </div>
+    <nav className='navbar'>
+      <div className='brand'>
+        <p>BestFit</p>
+      </div>
+      <ul className='navList'>
+        <li className='navItem'>Dashboard</li>
+        <li className='navItem'>Alunos</li>
+        <li className='navItem'>Treinos</li>
+        <li className='navItem'>Perfil</li>
+      </ul>
+    </nav>
   );
 };
 
