e2ccf5672625abf911cb3368a09b6ffc9b24970c
euluis-dev <fluisf00@gmail.com>
2024-09-27 18:06:14 -0300

    add feature: I created all the module.css files from the admin pages and imported them in their respective places

--- src/app/admin-users/page.js
+++ src/app/admin-users/page.js
@@ -1,14 +1,16 @@
 'use client';
 
-import AdminNavBar from '@/components/AdminNavBar';
+import AdminNavBar from '@/components/AdminNavBar'; 
+
+import styles from '../../styles/admin-users/page.module.css';
 
 const page = () => {
   return (
-    <div>
+    <div className={styles['users-container']}>
       <div><AdminNavBar /></div>
       <h1>alunosss</h1>
     </div>
-  )
-}
+  );
+};
 
 export default page;
--- src/app/admin-workout/page.js
+++ src/app/admin-workout/page.js
@@ -2,15 +2,17 @@
 
 import AdminNavBar from '@/components/AdminNavBar';
 
+import styles from '../../styles/admin-workout/page.module.css';
+
 import React from 'react'
 
 const page = () => {
   return (
-    <div>
+    <div className={styles['workout-container']}>
       <div><AdminNavBar /></div>
       <h1>workout</h1>
     </div>
   )
 }
 
-export default page
+export default page;
