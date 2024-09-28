5ac0732a1c377784fdf01b0bf41d9d41bcb5d25b
euluis-dev <fluisf00@gmail.com>
2024-09-28 09:22:44 -0300

    add feature: count-users route was created

---
+++ src/app/api/count-users/route.js
@@ -0,0 +1,23 @@
+import { MongoClient } from "mongodb"; 
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+    throw new Error('MongoDB URI não está definida.');
+};
+
+const client = new MongoClient(uri); 
+
+export async function GET() {
+    try {
+      await client.connect();
+      const db = client.db('BestFitData');
+      const collection = db.collection('users');
+
+      const countUsers = await collection.countDocuments({ status: 'aprovado' });
+      return new Response(JSON.stringfy({ cont: countUsers }), { status: 200 });
+        
+    } catch (error) {
+        return new Response(JSON.stringfy({ error: 'Erro ao contar usuários' }, { status: 500 }));  
+    };
+};
