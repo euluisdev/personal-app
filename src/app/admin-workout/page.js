de79a4bef0f19fa55e4602a2d8bccd2a2ad430aa
euluis-dev <fluisf00@gmail.com>
2024-10-02 12:52:37 -0300

    add feature: testing another way of generating training through artificial intelligence--install axios lib

--- src/app/admin-workout/page.js
+++ src/app/admin-workout/page.js
@@ -1,18 +1,75 @@
 'use client';
 
+import React, { useState } from 'react'; 
+import axios from 'axios'; 
+
 import AdminNavBar from '@/components/AdminNavBar';
 
 import styles from '../../styles/admin-workout/page.module.css';
 
-import React from 'react'
+const WorkoutGenerator = () => {
+   const [prompt, setPrompt] = useState(''); 
+  const [generatedWorkout, setGeneratedWorkout] = useState('');
+  const [isLoading, setIsLoading] = useState(false); 
 
-const page = () => {
+  const generateWorkout = async () => {
+     setIsLoading(true); 
+
+    try {
+    
+      const response = await axios.post(
+        'https://api.cohere.ai/v1/generate',
+        {
+          model: 'command-xlarge-nightly',
+          prompt: `Crie um treino de musculação com base nas seguintes informações: ${prompt}. 
+                   Inclua apenas cinco exercícios específicos de cada músculo citado, número
+                   de séries, repetições, e a máquina que é para fazer o exercício. Apenas isso! sem instruções.`,
+          max_tokens: 300,
+          temperature: 0.5,
+          k: 0,
+          stop_sequences: [],
+          return_likelihoods: 'NONE'
+        },
+        {
+          headers: {
+            'Authorization': `BEARER ${process.env.NEXT_PUBLIC_COHERE_API_KEY}`,
+            'Content-Type': 'application/json'
+          }
+        }
+      );
+      console.log(response.data);
+
+      setGeneratedWorkout(response.data.generations[0].text);
+
+    } catch (error) {
+      console.error('Erro ao gerar treino:', error);
+      setGeneratedWorkout('Erro ao gerar o treino. Por favor, tente novamente.');
+    }
+  }; 
+
   return (
-    <div className={styles['workout-container']}>
+    <div className={styles['container']}>
       <div><AdminNavBar /></div>
       <h1>workout</h1>
-    </div>
-  )
-}
+      
+      <textarea
+        value={prompt}
+        onChange={(e) => setPrompt(e.target.value)}
+        placeholder="Descreva o treino desejado (ex: treino de peito e tríceps para intermediários)"
+      />
+      <button onClick={generateWorkout} disabled={isLoading}>
+        {isLoading ? 'Gerando...' : 'Gerar Treino'}
+      </button> 
 
-export default page;
+      {generatedWorkout && (
+        <div>
+          <h3>Treino Gerado:</h3>
+          <p>{generatedWorkout}</p>
+        </div>
+      )}
+
+    </div>
+  );
+};  
+
+export default WorkoutGenerator;
