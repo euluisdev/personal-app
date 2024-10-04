bb554a990c22582dcfb5c7a4f6a41a2ae30f70c2
euluis-dev <fluisf00@gmail.com>
2024-10-04 09:25:44 -0300

    add feature: useeffect of calling the API thet will fetch data from the mongodb database--API not yet created

--- src/app/admin-profile/page.js
+++ src/app/admin-profile/page.js
@@ -1,10 +1,44 @@
 'use client';
 
 import AdminNavBar from '@/components/AdminNavBar';
+import { useState, useEffect } from 'react';
 
 import styles from '../../styles/admin-profile/page.module.css'
 
-const page = () => {
+const AdminProfile = () => {
+  const [profileData, setProfileData] = useState(null); 
+  const [isEditing, setIsEditing] = useState(false);
+  const [formData, setFormData] = useState({
+    name: '', 
+    email: '', 
+    bio: '',
+  });
+
+  useEffect(() => {
+    const fetchProfile = async () => {
+      try {
+        const response = await fetch('/api/admin-profile'); 
+        const data = await response.json();
+        console.log(data);
+        if (data) {
+          setProfileData(data);
+          setFormData({
+            name: data.name, 
+            email: data.email, 
+            bio: data.bio,
+          });
+        } else {
+          console.error(`Error na resposta da API!`);
+        }
+
+      } catch (error) {
+        console.error('Error fetching profile data:', error);
+      }
+    };
+
+    fetchProfile();
+  }, []);
+
   return (
     <div className={styles['profile-container']}>
       <div><AdminNavBar /></div>
@@ -13,4 +47,4 @@ const page = () => {
   );
 };
 
-export default page;
+export default AdminProfile;
