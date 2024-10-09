47b733becc714a1afe103d74ee6757a0bff81eaa
euluis-dev <fluisf00@gmail.com>
2024-10-09 19:28:31 -0300

    add feature: Add OPTIONS handler to enable CORS support for various HTTP methods

--- src/app/api/admin-profile/route.js
+++ src/app/api/admin-profile/route.js
@@ -98,3 +98,14 @@ export async function PUT(req) {
     }
 };  
 
+export function OPTIONS() {
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+      }
+    });
+  }
+
