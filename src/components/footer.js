601db8d8d4219de41e6f780bf9982fb15a7a041d
euluis-dev <fluisf00@gmail.com>
2024-10-14 18:14:27 -0300

    add feature: footer component was styled with the footer.css file

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,20 @@
-import {
-    BsLinkedin,
-    BsGithub,
-    BsInstagram,
-} from 'react-icons/bs';
-
+import { BsLinkedin, BsGithub, BsInstagram } from 'react-icons/bs';
 import './footer.css';
 
 const Footer = () => {
   return (
     <footer className="footer-container">
-        <div className="footer-icons">
-            <ul>
-                <li><a href="https://www.linkedin.com/in/luis-figueiredo-077aba1a3/"><BsLinkedin /></a></li> 
-                <li><a href="https://github.com/euluisdev"><BsGithub /></a></li> 
-                <li> <a href="https://github.com/euluisdev"><BsInstagram /></a></li> 
-            </ul>
-        </div>
-        <div className="developed">
-            <p>Desenvolvido por: <a href="https://github.com/euluisdev">euluis-dev</a></p> 
-            <p>Todos os direitos reservados: &copy;2024 BestFit</p> 
-        </div>
+      <div className="footer-icons">
+        <ul>
+          <li><a href="https://www.linkedin.com/in/luis-figueiredo-077aba1a3/" aria-label="LinkedIn"><BsLinkedin /></a></li>
+          <li><a href="https://github.com/euluisdev" aria-label="GitHub"><BsGithub /></a></li>
+          <li><a href="https://www.instagram.com/seuinstagram" aria-label="Instagram"><BsInstagram /></a></li>
+        </ul>
+      </div>
+      <div className="developed">
+        <p>Desenvolvido por <a href="https://github.com/euluisdev">euluis-dev</a></p>
+        <p>&copy; 2024 BestFit. Todos os direitos reservados.</p>
+      </div>
     </footer>
   )
 };
