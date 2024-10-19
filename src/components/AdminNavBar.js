fab3ad7d7fdaec006e53db7758dd4eb01d52fa38
euluis-dev <fluisf00@gmail.com>
2024-10-19 19:55:09 -0300

    add feature: I added the admin's profile photo to the navbar -- I took advantage of the API itself, which tries to display the image on the profile -- I made a query and just displayed the image -- and I also configured the .css of the feature

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -1,9 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import './AdminNavBar.css';
 
 const AdminNavBar = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [profilePhoto, setProfilePhoto] = useState(null); 
+    const [isLoading, setIsLoading] = useState(false);  
+
+    useEffect(() => {
+        const fetchProfilePhoto = async () => {
+            setIsLoading(true);
+            try {
+                const response = await fetch('api/admin-profile');
+                const data = await response.json(); 
+
+                if (response.ok) {
+                    setProfilePhoto(data)
+                } else {
+                    console.error(`Erro na resposta da API!`, data);
+                }
+
+            } catch (error) {
+                console.error('Erro ao buscar file:', error); 
+
+            } finally {
+                setIsLoading(false);
+            }
+        };
+        fetchProfilePhoto();   
+    }, [])
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
@@ -33,6 +58,17 @@ const AdminNavBar = () => {
                     <Link href="/admin-profile">Perfil</Link>
                 </li>
             </ul>
+
+            {isLoading && <div className='loadingSpinner'/>}
+
+            {profilePhoto && (
+              <img 
+                src={profilePhoto.photoUrl} 
+                alt="Foto do Perfil"    
+                className='profileImage'
+              />
+            )}
+
         </nav>
     );
 };
