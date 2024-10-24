2173884234bd6d2d02510426b72d7f7170e31357
euluis-dev <fluisf00@gmail.com>
2024-10-23 21:08:32 -0300

    add feat(api): Implemented password reset functionality -- generating a password reset token using /crypto.randomBytes/ and setting the expiration to 1 hour -- updating the user document in the database with the reset token and the expiration date -- I configured the email transport using /nodemailer/ allowing emails to be sent via SMTP -- this sends an email to the user containing a link to reset the password, which includes the token generated -- returning a next-JSON response indicating that the email with the link was sent -- these implementations ensure that users can reset their passwords efficiently -- flat create the reset page

--- src/app/api/user-forgot-password/route.js
+++ src/app/api/user-forgot-password/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server"; 
 import nodemailer from "nodemailer"; 
 import crypto from "crypto";  
-import { MongoClient } from "mongodb"; 
+import { Collection, MongoClient } from "mongodb"; 
 
 const uri = process.env.MONGODB_URI;
 
@@ -28,9 +28,46 @@ export async function POST (req) {
             );
         };  
 
+        const resetToken = crypto.randomBytes(32).toString('hex');  
+        const resetTokenExpiry = new Date(Date.now() + 3600000);  
+
+        await usersCollection.updateOne(
+            { email }, 
+            {
+              $set: { resetToken, resetTokenExpiry }  
+            }, 
+        );
+
+        const transporter = nodemailer.createTransport({
+          host: process.env.SMTP_HOST,
+          port: process.env.SMTP_PORT,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASSWORD,
+          },
+        });
+
+        await transporter.sendMail({
+            from: process.env.SMTP_FROM,
+            to: email,
+            subject: 'Password Reset Request',
+            html: `
+              <h1>Password Reset Request</h1>
+              <p>Click the link below to reset your password. This link will expire in 1 hour.</p>
+              <a href="${process.env.NEXT_PUBLIC_APP_URL}/reset-password?token=${resetToken}">
+                Reset Password
+              </a>
+            `,
+          });
+
+        return NextResponse.json(
+          { message: 'Um link de redefinição de senha foi enviado para seu email.' },
+          { status: 200 }
+        );
+
     } catch (error) {
         return NextResponse.json(
-            { message: 'An error occurred while processing your request.' },
+            { message: 'An error occurred while processing your request.', error },
             { status: 500 }
         ); 
     };
