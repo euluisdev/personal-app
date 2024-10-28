6eaffd7cc4e79be166700009d7f2e17db8cb8acc
euluis-dev <fluisf00@gmail.com>
2024-10-28 11:14:14 -0300

    fix bug: I was accessing the .value property of the /updatedWorkout/ object and including only this property as a response, as a result, the data was changed in the database, but I received status400, the page was not found -- I decided to pass the entire object/ updatedWorkout/ in the response and I no longer received the error

---
+++ src/app/api/user-update-workout/route.js
@@ -0,0 +1,43 @@
+import { MongoClient, ObjectId } from "mongodb";
+import { NextResponse } from 'next/server';
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error('MongoDB URI não está definida.');
+}
+
+const client = new MongoClient(uri);
+
+export async function POST(req) {
+  try {
+    await client.connect();
+    const db = client.db('BestFitData');
+    const workoutsCollection = db.collection('workouts');
+
+    const { workoutId, status } = await req.json();
+
+    if (!workoutId || !status) {
+      return NextResponse.json({ message: 'Parâmetros inválidos' }, { status: 400 });
+    }
+
+    const updatedWorkout = await workoutsCollection.findOneAndUpdate(
+      { _id: new ObjectId(workoutId) },
+      { $set: { workoutStatus: status } },
+      { returnDocument: 'after' }
+    );
+
+    if (!updatedWorkout) {
+      return NextResponse.json({ message: 'Treino não encontrado' }, { status: 404 });
+    }
+
+    return NextResponse.json({ message: 'Status atualizado com sucesso', workout: updatedWorkout }, { status: 200 });
+  } catch (error) {
+    console.error('Erro ao atualizar o status do treino:', error);
+    return NextResponse.json({ message: 'Erro no servidor ao atualizar o status' }, { status: 500 });
+  } finally {
+    await client.close();
+  };  
+};  
+ 
+ 
