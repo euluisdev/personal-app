c12b2c98dc8719270b15273ef4b1286e03e049e8
euluis-dev <fluisf00@gmail.com>
2024-11-15 17:19:06 -0300

    change link instagram

--- src/components/footer.js
+++ src/components/footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <ul>
           <li><a href="https://www.linkedin.com/in/luis-figueiredo-077aba1a3/" aria-label="LinkedIn"><BsLinkedin /></a></li>
           <li><a href="https://github.com/euluisdev" aria-label="GitHub"><BsGithub /></a></li>
-          <li><a href="https://www.instagram.com/seuinstagram" aria-label="Instagram"><BsInstagram /></a></li>
+          <li><a href="https://www.instagram.com" aria-label="Instagram"><BsInstagram /></a></li>
         </ul>
       </div>
       <div className="developed">
